7e9cc1d039ecdd22acc098a578ac15eb3c16159f
mariyam-190 <mariyam_190@yahoo.com>
2023-10-11 09:02:25 +0300

    Initial Commit

---
+++ drawApp/drawAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawAppApp.swift
+//  drawApp
+//
+//  Created by mariyam yasin on 26/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct drawAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
